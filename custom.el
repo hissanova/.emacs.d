1a4304a860fea2ed00a4dde25b51159d3e928732
hissanova <369bodhisattva@gmail.com>
2021-04-09 11:29:11 +0900

    changes to custom.el

--- custom.el
+++ custom.el
@@ -4,14 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("444238426b59b360fb74f46b521933f126778777c68c67841c31e0a68b0cc920" default)))
+   '("444238426b59b360fb74f46b521933f126778777c68c67841c31e0a68b0cc920" default))
  '(org-agenda-files
-   (quote
-    ("~/Documents/math_notes/reading-notes/regular-polytopes.org" "~/projects/e-toyota_gan/kci-gan/tcspcs-release201903/scripts/descriptions.org")))
+   '("~/Documents/math_notes/reading-notes/regular-polytopes.org" "~/projects/e-toyota_gan/kci-gan/tcspcs-release201903/scripts/descriptions.org"))
  '(package-selected-packages
-   (quote
-    (web-mode use-package py-yapf yaml-mode cdlatex buffer-expose expand-region paredit cider zenburn-theme slime sclang-extensions rope-read-mode py-autopep8 pipenv nhexl-mode material-theme magit intero flycheck-mypy elpy elisp-slime-nav ein dired-subtree diminish company-auctex better-defaults))))
+   '(mozc moz fcitx web-mode use-package py-yapf yaml-mode cdlatex buffer-expose expand-region paredit cider zenburn-theme slime sclang-extensions rope-read-mode py-autopep8 pipenv nhexl-mode material-theme magit intero flycheck-mypy elpy elisp-slime-nav ein dired-subtree diminish company-auctex better-defaults)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
